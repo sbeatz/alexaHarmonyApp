948b174bcdc594ea6a315021ce791e72876b9165
sbeatz <sbeatz76@googlemail.com>
2016-11-18 07:35:27 +0100

    Update remote_conf.js

--- remote_conf.js
+++ remote_conf.js
@@ -1,17 +1,16 @@
 module.exports = {
-    hub_ip : '',
-    app_id : 'amzn1.echo-sdk-ams.app.999999-d0ed-9999-ad00-999999d00ebe'//,
-//    Optional channel configuration
-//    channels : [
-//         {
-//           utterance_name : 'watch NBC',
-//           activity: 'Watch Tivo',
-//           channel: "516"
-//        },
-//        {
-//           utterance_name : 'watch TBS',
-//           activity: 'Watch Tivo',
-//           channel: "552"
-//        }
-//    ]
+    hub_ip : '192.168.2.158',
+    app_id : 'amzn1.ask.skill.2d0c0cde-4386-474e-91c4-7c33a81ab318'//,
+    channels : [
+         {
+           utterance_name : 'schalte um ARD',
+           activity: 'Fernsehen',
+           channel: "1"
+        },
+        {
+           utterance_name : 'schalte um auf ZDF',
+           activity: 'Fernsehen',
+           channel: "2"
+        }
+    ]
 };
